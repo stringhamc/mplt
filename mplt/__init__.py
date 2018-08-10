60c401f4ef62a6e66d2188cd1c574d7d04df6eda
mizzbrumblebee <taylerbrumble@ou.edu>
2018-08-10 10:51:28 -0700

    Took out RC Params dictionary
    Don't see instance of this dictionary being called. The previous call to 'linecolors' broke the code.

--- mplt/__init__.py
+++ mplt/__init__.py
@@ -12,13 +12,6 @@ import numpy as np
 import os
 import errno
 
-# linecolors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
-#              '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
-rcParams['axes.prop_cycle'] = (
-    "cycler('linestyle', ['-', '--', ':']) * cycler('color', {})".format(linecolors))
-rcParams['image.aspect'] = 'auto'
-
-
 def myfig(x=None, showtitle=True, clearfig=True, **kwargs):
     if x is None:
         x = np.random.randint(10000)
