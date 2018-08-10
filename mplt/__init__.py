67bb4007a8d905563ff5cfc4cc66f2efe0909031
stringhamc <cstring@byu.net>
2018-08-09 17:33:44 -0700

    Did os import

--- mplt/__init__.py
+++ mplt/__init__.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.path import Path as mpPath
 import numpy as np
+import os
 
 linecolors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',
               '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
