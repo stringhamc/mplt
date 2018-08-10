bdedda4493167e57dde47b01501550d38d06d49e
Craig Stringham <craig@capellaspace.com>
2018-08-10 11:44:52 -0700

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mplt',
-      version='2.0.3',
+      version='2.0.5',
       description='Set of niceties wrapping matplotlib for signal processing',
       url='http://github.com/stringhamc/mplt',
       author='Craig Stringham',
